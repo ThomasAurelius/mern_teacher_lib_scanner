c70f02c9bc04a59c19b40ec1808be544b8021ccd
Thomas Aurelius <teg824@hotmail.com>
2022-07-13 21:56:38 -0500

    removed bookmodal2

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import AddBookModal2 from '../components/AddBookModal2'
 import AddBookModalSpeechly from '../components/AddBookModalSpeechly'
 import AddBookModalScanner from '../components/AddBookModalScanner'
 import Books from '../components/Books'
